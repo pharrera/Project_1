d4bd2588928770cf5eebef42953b569c8468a034
bobbystunes <145064892+bobbystunes@users.noreply.github.com>
2023-09-15 20:27:19 -0700

    Update quick_sort.c
    I'm not too sure how this works, but I am doing quick sort in C.

--- .vscode/quick_sort.c
+++ .vscode/quick_sort.c
@@ -3,9 +3,43 @@
 // void quick_sort(int *a, int n);   // merge sort an array a with n elements in C
 
 
+void swap(int array[], int i, int j) {
+    int temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
+
+void sort(int array[], int a, int b) {
+    if (a >= b)
+        return;
+
+    int pivot = array[a];
+    int i = a + 1, j = b;
+    while (i < j) {
+        while (i < j && array[j] >= pivot)
+            --j;
+        while (i < j && array[i] <= pivot)
+            ++i;
+        if (i < j)
+            swap(array, i, j);
+    }
+    if (array[a] > array[i]) {
+        swap(array, a, i);
+        sort(array, a, i - 1);
+        sort(array, i + 1, b);
+    } else { 
+        sort(array, a + 1, b);
+    }
+}
+
+void quicksort(int *a, int n) {
+    sort(*a, 0, n - 1);
+}
+
 int main() {
-  printf("Hello World!");
-  return 0;
+  int list[] = { 9, 2, 6, 7, 5, 4, 0, 2, 7, 5 };
+  
+  quicksort(list, 10);
 }
 
 
